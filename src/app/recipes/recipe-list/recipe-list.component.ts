d73872fbf39bcddd2a08bacab110010a95688a10
boby177 <bomasu37@gmail.com>
2022-06-15 10:38:48 +0700

    Cooking apps v 1.0

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'this is just a test', 'https://awsimages.detik.net.id/community/media/visual/2021/08/25/resep-nasi-goreng-sosis-ala-warung-bhakti_43.jpeg?w=700&q=90'),
+    new Recipe('Test Recipe', 'this is just a test', 'https://awsimages.detik.net.id/community/media/visual/2021/08/25/resep-nasi-goreng-sosis-ala-warung-bhakti_43.jpeg?w=700&q=90')
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
