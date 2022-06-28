98bb99ed626865dc9b499eeb6c301026efd94d6b
boby177 <bomasu37@gmail.com>
2022-06-28 15:16:15 +0700

    Section 18 Http Requests Api

---
+++ src/app/posts.service.ts
@@ -0,0 +1,92 @@
+import {
+  HttpClient,
+  HttpEventType,
+  HttpHeaders,
+  HttpParams,
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
+import { map, catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+
+@Injectable({ providedIn: "root" })
+export class PostService {
+  error = new Subject<string>();
+
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = { title: title, content: content };
+    this.http
+      .post<{ name: string }>(
+        "https://ng-complete-guide-187aa-default-rtdb.firebaseio.com/posts.json",
+        postData,
+        {
+          observe: "response",
+        }
+      )
+      .subscribe(
+        (responseData) => {
+          console.log(responseData);
+        },
+        (error) => {
+          this.error.next(error.message);
+        }
+      );
+  }
+
+  fetchPosts() {
+    let searchparams = new HttpParams();
+    searchparams.append("print", "bob");
+    searchparams = searchparams.append("custom", "key");
+    return (
+      this.http
+        .get<{ [key: string]: Post }>(
+          "https://ng-complete-guide-187aa-default-rtdb.firebaseio.com/posts.json",
+          {
+            headers: new HttpHeaders({ "Custom-Header": "Bob" }),
+            params: searchparams,
+            responseType: 'json'
+          }
+        )
+        // Convert or transform the js object into the data
+        .pipe(
+          map((responseData) => {
+            const postsArray: Post[] = [];
+            for (const key in responseData) {
+              if (responseData.hasOwnProperty(key)) {
+                postsArray.push({ ...responseData[key], id: key });
+              }
+            }
+            return postsArray;
+          }),
+          catchError((errorRes) => {
+            // Send to analytics server
+            return throwError(errorRes);
+          })
+        )
+    );
+  }
+
+  deletePosts() {
+    return this.http
+      .delete(
+        "https://ng-complete-guide-187aa-default-rtdb.firebaseio.com/posts.json",
+        {
+          observe: "events",
+          responseType: 'text'
+        }
+      )
+      .pipe(
+        tap((event) => {
+          console.log(event);
+          if (event.type === HttpEventType.Sent) {
+            // ...
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log(event.body);
+          }
+        })
+      );
+  }
+}
